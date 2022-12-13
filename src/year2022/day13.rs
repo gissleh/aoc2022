07c8ba9412f200fd4cd24981f442f5f7c7b46153
Gisle Aune <dev@gisle.me>
2022-12-13 20:55:31 +0100

    add 2022 day 13 (ugh)

--- src/year2022/day13.rs
+++ src/year2022/day13.rs
@@ -11,7 +11,7 @@ pub fn main(day: &mut Day, input: &[u8]) {
     day.note("Packet Terms", input.iter().map(|p| p.terms.len()).sum::<usize>());
 
     day.run(1, "", 100000, || part1(&input));
-    //day.run(2, "", 10000, || part2(&input));
+    day.run(2, "", 10000, || part2(&input));
 }
 
 fn parse(data: &[u8]) -> Vec<Packet> {
@@ -21,21 +21,37 @@ fn parse(data: &[u8]) -> Vec<Packet> {
         .parse(data).unwrap()
 }
 
-fn part1(input: &[Packet]) -> usize {
-    input.array_chunks::<2>()
+fn part1(packets: &[Packet]) -> usize {
+    packets.array_chunks::<2>()
         .enumerate()
-        .filter(|(_, [a, b])| b > a)
+        .filter(|(_, [a, b])| a < b)
         .map(|(i, _)| i + 1)
         .sum()
 }
 
-fn part2(input: &[u8]) -> u32 {
+fn part2(packets: &[Packet]) -> usize {
+    let mut packets = packets.to_vec();
+    packets.push(Packet::new_divider(2));
+    packets.push(Packet::new_divider(6));
+    packets.sort();
+
+    let mut divider_2_pos = 0;
+    for (i, packet) in packets.iter().enumerate() {
+        if let Some(divider) = packet.get_divider() {
+            if divider == 2 {
+                divider_2_pos = i + 1;
+            } else if divider == 6 {
+                return divider_2_pos * (i + 1)
+            }
+        }
+    }
+
     0
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Clone)]
 struct Packet {
-    terms: SmallVec<[PacketTerm; 64]>,
+    terms: SmallVec<[PacketPart; 64]>,
 }
 
 impl PartialOrd<Self> for Packet {
@@ -62,15 +78,15 @@ impl Packet {
         let ot = other.terms[ot_index];
 
         match st {
-            PacketTerm::List(sp, sl) => {
+            PacketPart::List(sp, sl) => {
                 let (mut sp, sl) = (sp as usize, sl as usize);
                 let (mut op, ol) = match ot {
-                    PacketTerm::List(op, ol) => (op as usize, ol as usize),
-                    PacketTerm::Number(_) => (ot_index, ot_index + 1),
+                    PacketPart::List(op, ol) => (op as usize, ol as usize),
+                    PacketPart::Number(_) => (ot_index, ot_index + 1),
                 };
 
                 loop {
-                    #[cfg(test)] println!("  s: {} {}, o: {} {}", sp, sl, op, ol);
+                    #[cfg(test)] println!("{} {}:  s: {} {}, o: {} {}", st_index, ot_index, sp, sl, op, ol);
 
                     // Stop if one of the lists have run out.
                     if sp >= sl && op == ol {
@@ -91,17 +107,39 @@ impl Packet {
                     }
                 }
             }
-            PacketTerm::Number(sv) => {
+            PacketPart::Number(sv) => {
                 match ot {
-                    PacketTerm::Number(ov) => {
+                    PacketPart::Number(ov) => {
                         (sv.cmp(&ov), st_index + 1, ot_index + 1)
                     }
-                    PacketTerm::List(..) => {
+                    PacketPart::List(..) => {
                         let (ordering, next_si, next_oi) = other.compare_list(self, ot_index, st_index);
-                        (ordering.reverse(), next_si, next_oi)
+                        (ordering.reverse(), next_oi, next_si) // They next's need to be swapped here too.
                     }
                 }
             }
+        }
+    }
+
+    fn get_divider(&self) -> Option<u16> {
+        if self.terms.len() == 3
+            && self.terms[0] == PacketPart::List(1, 3)
+            && self.terms[1] == PacketPart::List(2, 3) {
+            if let PacketPart::Number(v) = self.terms[2] {
+                return Some(v);
+            }
+        }
+
+        None
+    }
+
+    fn new_divider(v: u16) -> Packet {
+        Packet {
+            terms: smallvec![
+                PacketPart::List(1, 3),
+                PacketPart::List(2, 3),
+                PacketPart::Number(v)
+            ],
         }
     }
 
@@ -119,27 +157,27 @@ impl Packet {
             unsigned_int().skip(b',').skip(b' ').map(Token::Number),
         )).repeat_fold_mut(
             || {
-                let mut terms: SmallVec<[PacketTerm; 64]> = SmallVec::new();
-                let mut stack: SmallVec<[usize; 16]> = SmallVec::new();
+                let terms: SmallVec<[PacketPart; 64]> = SmallVec::new();
+                let stack: SmallVec<[usize; 16]> = SmallVec::new();
 
                 (terms, stack)
             },
-            |(terms, stack): &mut (SmallVec<[PacketTerm; 64]>, SmallVec<[usize; 16]>), token: Token| {
+            |(terms, stack), token| {
                 let index = terms.len() as u8;
 
                 match token {
                     Token::ListStart => {
-                        terms.push(PacketTerm::List(index + 1, 0));
+                        terms.push(PacketPart::List(index + 1, 0));
                         stack.push(terms.len() - 1);
                     }
                     Token::ListEnd => {
                         let list_end_index = stack.pop().unwrap();
-                        if let PacketTerm::List(_, end) = &mut terms[list_end_index] {
+                        if let PacketPart::List(_, end) = &mut terms[list_end_index] {
                             *end = index;
                         }
                     }
                     Token::Number(v) => {
-                        terms.push(PacketTerm::Number(v));
+                        terms.push(PacketPart::Number(v));
                     }
                 }
             },
@@ -148,7 +186,7 @@ impl Packet {
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
-enum PacketTerm {
+enum PacketPart {
     Number(u16),
     List(u8, u8),
 }
@@ -185,9 +223,58 @@ mod tests {
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 ";
 
+    const P1_EXAMPLE_REV: &[u8] = b"[1,1,5,1,1]
+[1,1,3,1,1]
+
+[[1],4]
+[[1],[2,3,4]]
+
+[[8,7,6]]
+[9]
+
+[[4,4],4,4,4]
+[[4,4],4,4]
+
+[7,7,7]
+[7,7,7,7]
+
+[3]
+[]
+
+[[]]
+[[[]]]
+
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+";
+
     #[test]
     fn part1_works_on_example() {
         assert_eq!(part1(&parse(P1_EXAMPLE)), 13);
+        assert_eq!(part1(&parse(P1_EXAMPLE_REV)), 23);
+    }
+
+    #[test]
+    fn parse_parses_multiple_packets() {
+        let packets = parse(P1_EXAMPLE);
+
+        assert_eq!(packets.len(), 16);
+        assert_eq!(packets[0], p(b"[1,1,3,1,1]"));
+        assert_eq!(packets[1], p(b"[1,1,5,1,1]"));
+        assert_eq!(packets[2], p(b"[[1],[2,3,4]]"));
+        assert_eq!(packets[3], p(b"[[1],4]"));
+        assert_eq!(packets[4], p(b"[9]"));
+        assert_eq!(packets[5], p(b"[[8,7,6]]"));
+        assert_eq!(packets[6], p(b"[[4,4],4,4]"));
+        assert_eq!(packets[7], p(b"[[4,4],4,4,4]"));
+        assert_eq!(packets[8], p(b"[7,7,7,7]"));
+        assert_eq!(packets[9], p(b"[7,7,7]"));
+        assert_eq!(packets[10], p(b"[]"));
+        assert_eq!(packets[11], p(b"[3]"));
+        assert_eq!(packets[12], p(b"[[[]]]"));
+        assert_eq!(packets[13], p(b"[[]]"));
+        assert_eq!(packets[14], p(b"[1,[2,[3,[4,[5,6,7]]]],8,9]"));
+        assert_eq!(packets[15], p(b"[1,[2,[3,[4,[5,6,0]]]],8,9]"));
     }
 
     #[test]
@@ -252,6 +339,14 @@ mod tests {
         assert_eq!(p(b"[[1],4]").cmp(&p(b"[[1],[2,3,4]]")), Ordering::Greater);
         assert_eq!(p(b"[15]").cmp(&p(b"[17]")), Ordering::Less);
         assert_eq!(p(b"[[],[],117]").cmp(&p(b"[[],[],[]]")), Ordering::Greater);
+        assert_eq!(p(b"[[9]]").cmp(&p(b"[[[9]]]")), Ordering::Equal);
+        assert_eq!(p(b"[[9]]").cmp(&p(b"[[[[9]]]]")), Ordering::Equal);
+        assert_eq!(p(b"[[[[9]]],[],[9,[8],8],[[7,[],4],8,[[],1,[0,4,7],[3,10,6]]]]").cmp(
+            &p(b"[[9],[5,[6,7],1,7],[[[0],9,1,[]]]]")
+        ), Ordering::Less);
+        assert_eq!(p(b"[[9],[5,[6,7],1,7],[[[0],9,1,[]]]]").cmp(
+            &p(b"[[[[9]]],[],[9,[8],8],[[7,[],4],8,[[],1,[0,4,7],[3,10,6]]]]")
+        ), Ordering::Greater);
     }
 
     #[test]
@@ -260,7 +355,7 @@ mod tests {
             Packet::parser().parse(b"[9]"),
             ParseResult::Good(Packet {
                 terms: smallvec![
-                    PacketTerm::List(1, 2), PacketTerm::Number(9),
+                    PacketPart::List(1, 2), PacketPart::Number(9),
                 ]
             }, b"")
         );
@@ -268,10 +363,10 @@ mod tests {
             Packet::parser().parse(b"[[1,2],3,4,5]"),
             ParseResult::Good(Packet {
                 terms: smallvec![
-                    PacketTerm::List(1, 7),
-                    PacketTerm::List(2, 4),
-                    PacketTerm::Number(1), PacketTerm::Number(2),
-                    PacketTerm::Number(3), PacketTerm::Number(4), PacketTerm::Number(5),
+                    PacketPart::List(1, 7),
+                    PacketPart::List(2, 4),
+                    PacketPart::Number(1), PacketPart::Number(2),
+                    PacketPart::Number(3), PacketPart::Number(4), PacketPart::Number(5),
                 ]
             }, b"")
         );
@@ -279,20 +374,20 @@ mod tests {
             Packet::parser().parse(b"[1,[2,[3,[4,[5,6,7]]]],8,9]"),
             ParseResult::Good(Packet {
                 terms: smallvec![
-                    PacketTerm::List(1, 14),
-                        PacketTerm::Number(1),
-                        PacketTerm::List(3, 12),
-                            PacketTerm::Number(2),
-                            PacketTerm::List(5, 12),
-                                PacketTerm::Number(3),
-                                PacketTerm::List(7, 12),
-                                    PacketTerm::Number(4),
-                                    PacketTerm::List(9, 12),
-                                        PacketTerm::Number(5),
-                                        PacketTerm::Number(6),
-                                        PacketTerm::Number(7),
-                    PacketTerm::Number(8),
-                    PacketTerm::Number(9),
+                    PacketPart::List(1, 14),
+                        PacketPart::Number(1),
+                        PacketPart::List(3, 12),
+                            PacketPart::Number(2),
+                            PacketPart::List(5, 12),
+                                PacketPart::Number(3),
+                                PacketPart::List(7, 12),
+                                    PacketPart::Number(4),
+                                    PacketPart::List(9, 12),
+                                        PacketPart::Number(5),
+                                        PacketPart::Number(6),
+                                        PacketPart::Number(7),
+                    PacketPart::Number(8),
+                    PacketPart::Number(9),
                 ]
             }, b"")
         );
@@ -300,10 +395,10 @@ mod tests {
             Packet::parser().parse(b"[[2],1]"),
             ParseResult::Good(Packet {
                 terms: smallvec![
-                    PacketTerm::List(1, 4),
-                    PacketTerm::List(2, 3),
-                    PacketTerm::Number(2),
-                    PacketTerm::Number(1),
+                    PacketPart::List(1, 4),
+                    PacketPart::List(2, 3),
+                    PacketPart::Number(2),
+                    PacketPart::Number(1),
                 ]
             }, b"")
         );
